b42253f3a05e47d11a5c484cade0f4cee5ad86b9
Junjie.Shi <shijunjiee@enn.cn>
2021-09-28 10:44:02 +0800

    Initial commit

---
+++ src/main/java/me/shijunjie/springbootdroolsdemo/SpringbootDroolsDemoApplication.java
@@ -0,0 +1,13 @@
+package me.shijunjie.springbootdroolsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDroolsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDroolsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/shijunjie/springbootdroolsdemo/SpringbootDroolsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.shijunjie.springbootdroolsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDroolsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
